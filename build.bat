3bcd70902af9179a5cdf43cd7965ee7212cc4e27
5qol255 <149298356+5qol255@users.noreply.github.com>
2025-09-15 22:01:47 +0800

    v0.3.1 更新构建脚本以使用正确的可执行文件名称，并更新日志文件

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
-python -m PyInstaller --onefile --noconsole --name "ForceDelete.exe" main.py
-move .\dist\ForceDelete.exe .\
-ForceDelete.exe build
-ForceDelete.exe dist
-ForceDelete.exe ForceDelete.exe.spec
+python -m PyInstaller --onefile --noconsole --name "FileEater.exe" main.py
+move .\dist\FileEater.exe .\
+FileEater.exe build
+FileEater.exe dist
+FileEater.exe FileEater.exe.spec
